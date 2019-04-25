343d6b013a26225af301297255c7a90e5e7d1634
arunvinesh81 <arunvines81@gmail.com>
2019-04-25 23:01:53 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* tab  */
+
+
+$(document).ready(function(){
+  $('ul.tabs').tabs();
+});
+$(document).ready(function(){
+  $('ul.tabs').tabs('select_tab', 'tab_id');
+});
+
+/*
+var comp = document.getElementById("componentName").click (function(){
+var testimonials = document.querySelectorAll('.component');
+Array.prototype.forEach.call(testimonials, function(elements, index) {
+   alert(elements.classList.item(1));
+});
+}*/
